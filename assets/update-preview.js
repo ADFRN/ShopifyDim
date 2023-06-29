7344fd7837d7792b60c84dbac0c68b197075143d
ADFRN <adrien.fournier1998@hotmail.fr>
2023-06-29 18:20:49 +0200

    test

--- assets/update-preview.js
+++ assets/update-preview.js
@@ -13,6 +13,34 @@ document.addEventListener("DOMContentLoaded", function() {
         typoValue = "Times-regular";
       }
       var couleurValue = document.getElementById("couleur").value;
+      if (couleurValue == "Noir") {
+        couleurValue = "black";
+      } else if (couleurValue == "Gris Foncé") {
+        couleurValue = "darkgray";
+      } else if (couleurValue == "Gris Moyen") {
+        couleurValue = "gray";
+      } else if (couleurValue == "Blanc") {
+        couleurValue = "white";
+      } else if (couleurValue == "Bleu Marine") {
+        couleurValue = "navy";
+      } else if (couleurValue == "Bleu Ciel") {
+        couleurValue = "skyblue";
+      } else if (couleurValue == "Rouge") {
+        couleurValue = "red";
+      } else if (couleurValue == "Rose") {
+        couleurValue = "hotpink";
+      } else if (couleurValue == "Rose Pâle") {
+        couleurValue = "pink";
+      } else if (couleurValue == "Marron") {
+        couleurValue = "brown";
+      } else if (couleurValue == "Jaune") {
+        couleurValue = "yellow";
+      } else if (couleurValue == "Orange") {
+        couleurValue = "orange";
+      } else if (couleurValue == "Doré") {
+        couleurValue = "gold";
+      }
+      
       var customizationTextFieldValue = document.getElementById("customizationTextField").value;
       customizationPreview.innerHTML = customizationTextFieldValue;
       customizationPreview.style.color = couleurValue;
